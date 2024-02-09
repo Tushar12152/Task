7988f4c2dc9b6c907c47fc8bcfe9ddf3c5fabce7
Tushar12152 <tusharimran470@gmail.com>
2024-02-09 20:10:13 +0600

    sort implement

--- src/Componets/UsersDetails.jsx
+++ src/Componets/UsersDetails.jsx
@@ -5,9 +5,87 @@ const UsersDetails = () => {
     const user=useLoaderData()
     // console.log(user);
     return (
-        <div>
-            
+       <div className="mt-20">
+
+
+
+
+
+
+         <div className="mt-10 w-[80%] mx-auto">
+                    
+
+        
+
+        <div className="border-2 border-b-0 border-black  w-full">
+        <figure>
+          <div className="avatar my-5 flex items-center justify-center">
+            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img src={user?.image} />
+            </div>
+          </div>
+        </figure>
+                         </div>
+
+
+                    <div className="flex w-">
+                         <div className="border-2 border-black  w-[50%]">
+                            <h1 className="text-center">First Name</h1>
+                         </div>
+                         
+                         <div className="border-2 border-l-0 border-black  w-[50%] ">
+                            <h1 className="text-center">{user?.firstName}</h1>
+                         </div>   
+                    </div>
+
+                    <div className="flex ">
+                         <div className="border-2 border-t-0 border-black  w-[50%]">
+                            <h1 className="text-center">Last Name</h1>
+                         </div>
+                         
+                         <div className="border-2 border-t-0 border-l-0 border-black  w-[50%] ">
+                            <h1 className="text-center">{user?.lastName}</h1>
+                         </div>   
+                    </div>
+
+                    <div className="flex ">
+                         <div className="border-2 border-t-0 border-black  w-[50%]">
+                            <h1 className="text-center">Email</h1>
+                         </div>
+                         
+                         <div className="border-2 border-t-0 border-l-0 border-black  w-[50%] ">
+                            <h1 className="text-center">{user?.email} </h1>
+                         </div>   
+                    </div>
+
+
+                    <div className="flex ">
+                         <div className="border-2 border-t-0 border-black  w-[50%]">
+                            <h1 className="text-center">Address</h1>
+                         </div>
+                         
+                         <div className="border-2 border-t-0 border-l-0 border-black  w-[50%] ">
+                               <div>
+                                     <h1 className="text-center">street: {user?.address?.address}</h1>
+                                     <h1 className="text-center">Suite: {user?.address?.state}</h1>
+                                     <h1 className="text-center">City: {user?.address?.city}</h1>
+                               </div>
+                         </div>   
+                    </div>
+
+                    <div className="flex ">
+                         <div className="border-2 border-t-0 border-black  w-[50%]">
+                            <h1 className="text-center">Company Name</h1>
+                         </div>
+                         
+                         <div className="border-2 border-t-0 border-l-0 border-black  w-[50%] ">
+                            <h1 className="text-center">{user?.company?.name} </h1>
+                          
+                         </div>   
+                    </div>
+
         </div>
+       </div>
     );
 };
 
--- src/Componets/UsersList.jsx
+++ src/Componets/UsersList.jsx
@@ -1,71 +1,92 @@
+
 import { useEffect, useState } from "react";
 import Title from "../Hooks/Title";
 import UsersCard from "./UsersCard";
 
 const UsersList = () => {
-const [allUsers,setAllUsers]=useState([])
+    const [allUsers, setAllUsers] = useState([]);
+    const [sortOrder, setSortOrder] = useState({ field: '', direction: 'asc' });
 
-
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://dummyjson.com/users')
-        .then(res=>res.json())
-        .then(data=>setAllUsers(data?.users))
-    },[])
+            .then(res => res.json())
+            .then(data => setAllUsers(data?.users))
+    }, []);
 
+    const handleSearch = e => {
+        e.preventDefault();
+        const name = e.target.keyword.value.toLowerCase();
 
+        if (name === '') {
+            return;
+        }
 
-//  console.log(allUsers);
-
-const handleSearch=e=>{
-     e.preventDefault()
-     const name=e.target.keyword.value;
-     console.log(name);
-
-    if(name===''){
-        return
+        // Filter the results
+        const searchResults = allUsers.filter(user => user.firstName.toLowerCase() === name);
+        setAllUsers(searchResults);
     }
-    
-    //filter because if one name used for tow object
-     const searchResults = allUsers.filter(user => user.firstName.toLowerCase() === name.toLowerCase())
-     setAllUsers(searchResults);
-    
-}
+
+    const handleSort = (field) => {
+        // Toggle the sort direction between 'asc' and 'desc' for the given field
+        const newDirection = sortOrder.field === field && sortOrder.direction === 'asc' ? 'desc' : 'asc';
+
+        // Sort the users based on the selected field
+        const sortedUsers = [...allUsers].sort((a, b) => {
+            const valueA = a[field].toLowerCase();
+            const valueB = b[field].toLowerCase();
+
+            if (newDirection === 'asc') {
+                return valueA.localeCompare(valueB);
+            } else {
+                return valueB.localeCompare(valueA);
+            }
+        });
+
+        setAllUsers(sortedUsers);
+        setSortOrder({ field, direction: newDirection });
+    }
 
     return (
         <div>
-             <Title heading={'Users'}></Title>
+            <Title heading={'Users'}></Title>
 
-
-               {/* /searchbox implemnt */}
-                <div className="my-10">
+           <div className="lg:flex justify-between items-center">
+             {/* Search box implementation */}
+             <div className="my-10">
                 <form onSubmit={handleSearch}>
-                    
-               <div className="flex items-center ">
-                   
-                    <input
-                        type="text"
-                        name="keyword"
-                        placeholder="Search by users first name"
-                        className="py-2 px-4 text-black mr-2 w-60 border rounded-l focus:outline-none"
-                    />
-                    <button type="submit" className="bg-blue-400 hover:bg-yellow-300 py-2 px-4 text-white rounded-r">
-                        Search
-                    </button>
-                </div>
-               </form>
-                </div>
-              
+                    <div className="flex items-center ">
+                        <input
+                            type="text"
+                            name="keyword"
+                            placeholder="Search by users first name"
+                            className="py-2 px-4 text-black mr-2 w-60 border rounded-l focus:outline-none"
+                        />
+                        <button type="submit" className="bg-blue-400 hover:bg-yellow-300 py-2 px-4 text-white rounded-r">
+                            Search
+                        </button>
+                    </div>
+                </form>
+            </div>
 
+            {/* Sort buttons */}
+            <div className="flex mb-4">
+                <button onClick={() => handleSort('firstName')} className="bg-gray-200 py-2 px-4 text-black rounded mr-2">
+                    Sort by Name {sortOrder.field === 'firstName' && sortOrder.direction === 'asc' ? '▲' : '▼'}
+                </button>
+                <button onClick={() => handleSort('email')} className="bg-gray-200 py-2 px-4 text-black rounded mr-2">
+                    Sort by Email {sortOrder.field === 'email' && sortOrder.direction === 'asc' ? '▲' : '▼'}
+                </button>
+                {/* <button onClick={() => handleSort('company')} className="bg-gray-200 py-2 px-4 text-black rounded">
+                    Sort by Company {sortOrder.field === 'company' && sortOrder.direction === 'asc' ? '▲' : '▼'}
+                </button> */}
+            </div>
 
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                 {
-                    allUsers?.map(user=><UsersCard key={UsersCard.id} card={user} />)
-                 }
-              </div>               
+           </div>
 
-
-
-
+            {/* User cards */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {allUsers?.map(user => <UsersCard key={user.id} card={user} />)}
+            </div>
         </div>
     );
 };
